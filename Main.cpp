1ceb102971e9c96118ccbe37c4694adf51c881f4
tamfign <tamfign@gmail.com>
2017-10-19 16:12:40 +1100

    Use cart

--- Main.cpp
+++ Main.cpp
@@ -7,15 +7,29 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+	MPI_Comm comm2d;
+	int up, down;
+	int dims[2];
+	int period[2];
 	int rank, size;
+
 	if (argc < 2) {
 		cout << "Please run with training file and test file as parameters" << endl;
 		return -1;
 	}
 
+	period[0] = 1;
+	period[1] = 1;
+	dims[0] = 0;
+	dims[1] = 0;
+
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	MPI_Dims_create(size, 2, dims);
+	MPI_Cart_create(MPI_COMM_WORLD, 2, dims, period, 1, &comm2d);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Cart_shift(comm2d, 1, 1, &down, &up);
 
 	ArffImporter* trainSetImporter = new ArffImporter(argv[1]);
 	ArffImporter* testSetImporter = new ArffImporter(argv[2]);
