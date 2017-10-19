fe56e0074b70349a8b884cfdff72dd6d36f67a12
tamfign <tamfign@gmail.com>
2017-10-19 17:12:47 +1100

    Use more taks

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/csh
 #SBATCH --time=1:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=2
+#SBATCH --nodes=10
+#SBATCH --ntasks=10
 #SBATCH --cpus-per-task=8
 #SBATCH --output="result"
 
 # Run info and srun job launch
-srun -n 2 ./exec Dataset/train/train-first10.arff Dataset/test/dev-first10.arff
+srun ./exec Dataset/train/train-first1000.arff Dataset/test/dev-first1000.arff
